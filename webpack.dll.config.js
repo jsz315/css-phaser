a47b067c609a130a9309a35d95ec16a03cd20340
jsz315 <jsz_thing@126.com>
2020-05-12 13:57:29 +0800

    修改dll名字

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -9,9 +9,9 @@ module.exports = {
         phaser: ['phaser']
     },
     output: {
-        filename: '[name].dll.[hash:8].js',
+        filename: '[name].dll.js',
         path: path.join(__dirname, 'public/dll'),
-        library: '_dll_[name]_[hash:8]'
+        library: '_dll_[name]'
     },
     plugins: [
         new CleanWebpackPlugin({
@@ -20,7 +20,7 @@ module.exports = {
             dry: false
         }),
         new webpack.DllPlugin({
-            name: '_dll_[name]_[hash:8]',
+            name: '_dll_[name]',
             path: path.join(__dirname, 'public/dll/[name].manifest.json')
         }),
         new BundleAnalyzerPlugin()
